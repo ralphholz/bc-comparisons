7aad5a595bbd5738d254e0cc3e18de1dbb38cd46
Lexi Brent <lexi.brent@sydney.edu.au>
2019-05-27 18:52:19 +1000

    Bugfixes etc

--- select_scanfiles.py
+++ select_scanfiles.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+import sys
 import csv
 import glob
 import logging
@@ -10,71 +12,85 @@ import collections
 from datetime import datetime
 from os import path
 
+import util
+
+logging.basicConfig(filename="select_scanfiles.log", 
+        format=util.LOG_FMT, level=logging.DEBUG)
+
 class ScansLoader:
-  def __init__(self, scans_dir):
-    self.scans_dir = scans_dir
-    self.scanfiles = self._list_scanfiles()
+    def __init__(self, scans_dir):
+        self.scans_dir = scans_dir
+        self.scanfiles = self._list_scanfiles()
+        logging.debug("Loaded scanfiles: %s", ";".join(self.scanfiles))
 
-  def _list_scanfiles(self):
-    raise NotImplementedError
-  
-  def filedt(self, scanfile):
-    """Extract UTC datetime from given scan file path"""
-    raise NotImplementedError
+    def _list_scanfiles(self):
+        raise NotImplementedError
 
-  def filter(self, not_before:str=None, not_after:str=None, custom=None):
-    """Remove scan files not meeting time restrictions or custom condition.
-    not_before and not_after should be type datetime in UTC."""
-    if not_before is None and not_after is None and custom is None:
-      return
-    filtered = self.scanfiles
-    if not_before is not None:
-      filtered = filter(lambda sf: self.filedt(sf) >= not_before, filtered)
-    if not_after is not None:
-      filtered = filter(lambda sf: self.filedt(sf) <= not_after, filtered)
-    if custom is not None:
-      filtered = filter(custom, filtered)
-    self.scanfiles = list(filtered)
+    def filedt(self, scanfile):
+        """Extract UTC datetime from given scan file path"""
+        raise NotImplementedError
 
-  def downsample(self, targets=("12:00:00")):
-    """Downsample scan files by taking nearest scan to each target time in
-    24-hour HH:MM:SS format in each UTC day. This is NOT order-preserving."""
-    def find_closest(scans, day, target):
-        target_dt = util.time2dt(target)
-        return min(scans, key=lambda s: abs(self.filedt(sf) - target_dt))
-    downsampled = []
-    sf_by_date = self.scanfiles_by_date()
-    for day, scans in sf_by_date.items():
-      closest = {find_closest(scans, day, target) for target in targets}
-      if len(closest) != len(targets):
-        logging.warning(day, "has only", len(closest), "of", len(targets),
-            "scans after downsampling!")
-      for sf in closest:
-        downsampled.append(sf)
-    self.scanfiles = downsampled()
+    def filter(self, not_before:str=None, not_after:str=None, custom=None):
+        """Remove scan files not meeting time restrictions or custom condition.
+        not_before and not_after should be type datetime in UTC."""
+        if not_before is None and not_after is None and custom is None:
+            return
+        filtered = self.scanfiles
+        if not_before is not None:
+            filtered = filter(lambda sf: self.filedt(sf) >= not_before, filtered)
+            logging.info("Filtering out scans before %s", not_before.isoformat())
+        if not_after is not None:
+            filtered = filter(lambda sf: self.filedt(sf) <= not_after, filtered)
+            logging.info("Filtering out scans after %s", not_after.isoformat())
+        if custom is not None:
+            logging.info("Running custom filter")
+            filtered = filter(custom, filtered)
+        self.scanfiles = list(filtered)
 
-  def __sort_scanfiles(self, scanfiles, reverse=False):
-    return sorted(self.scanfiles, key=lambda sf: self.filedt(sf), reverse=reverse)
+    def downsample(self, targets=("12:00:00")):
+        """Downsample scan files by taking nearest scan to each target time in
+        24-hour HH:MM:SS format in each UTC day. This is NOT order-preserving."""
+        def find_closest(scans, day, target):
+            target_dt = util.time2dt(target, day)
+            return min(scans, key=lambda sf: abs(self.filedt(sf) - target_dt))
+        downsampled = []
+        sf_by_date = self.scanfiles_by_date()
+        for day, scans in sf_by_date.items():
+            closest = {find_closest(scans, day, target) for target in targets}
+            if len(closest) != len(targets):
+                logging.warning("%s has only %s of %s scans after downsampling!", 
+                                day, len(closest), len(targets))
+            for sf in closest:
+                downsampled.append(sf)
+        self.scanfiles = downsampled
 
-  def sort(self, reverse=False):
-    """Sort the list of scan files by date"""
-    self.scanfiles = self.__sort_scanfiles()
+    def __sort_scanfiles(self, scanfiles, reverse=False):
+        return sorted(self.scanfiles, key=lambda sf: self.filedt(sf), reverse=reverse)
 
-  def scanfiles_by_date(self):
-    """Group scan files by ISO date string (YYYY-MM-DD), return a dict 
-    {date -> list of scanfiles}. This method is order-preserving."""
-    sf_by_date = collections.defaultdict(list)
-    for sf in self.scanfiles:
-      day = self.filedt(sf).isoformat().split("T")[0]
-      sf_by_date[day].append(sf)
-    return sf_by_date
+    def sort(self, reverse=False):
+        """Sort the list of scan files by date"""
+        logging.info("Sorting scan files (reverse={})".format(str(reverse)))
+        self.scanfiles = self.__sort_scanfiles(self.scanfiles)
+
+    def scanfiles_by_date(self):
+        """Group scan files by ISO date string (YYYY-MM-DD), return a dict 
+        {date -> list of scanfiles}. This method is order-preserving."""
+        sf_by_date = collections.defaultdict(list)
+        for sf in self.scanfiles:
+            day = self.filedt(sf).isoformat().split("T")[0]
+            sf_by_date[day].append(sf)
+        return sf_by_date
+
+    def scanfiles_and_isotimes(self):
+        for sf in self.scanfiles:
+            yield (self.filedt(sf).isoformat(), sf)
 
 class YethiScansLoader(ScansLoader):
-  FILE_GLOB = "*/confirmed.csv.xz"
-  def filedt(self, scanfile):
-    return datetime.utcfromtimestamp(int(scanfile.split('/')[-2]))
-  def _list_scanfiles(self):
-    return glob.glob(path.join(self.scans_dir, self.FILE_GLOB))
+    FILE_GLOB = "*/confirmed.csv.xz"
+    def filedt(self, scanfile):
+        return datetime.utcfromtimestamp(int(scanfile.split('/')[-2]))
+    def _list_scanfiles(self):
+        return glob.glob(path.join(self.scans_dir, self.FILE_GLOB))
 
 FORMAT_LOADERS = {
     "Yethi": YethiScansLoader,
@@ -84,9 +100,7 @@ parser = argparse.ArgumentParser()
 
 # Optional args
 parser.add_argument("--delimiter", "-d", default="\t",
-  help="Input and output field delimiter (tab by default)")
-parser.add_argument("--inner-delimiter", "-id", default=";", 
-  help="Delimiter to use for lists within a field (; by default)")
+  help="Output field delimiter (tab by default)")
 parser.add_argument("--not-before", "-nb", default=None,
   help="Don't include scan files before the given UTC ISO date/time string")
 parser.add_argument("--not-after", "-na", default=None,
@@ -101,9 +115,34 @@ parser.add_argument("--format", "-f", choices=list(FORMAT_LOADERS.keys()),
 parser.add_argument("scan_dir",
   help="Full path to directory containing scan files")
 
+logging.debug("===STARTUP===")
+
 ARGS = parser.parse_args()
+
+# Initialize TSV output writer
+writer = csv.writer(sys.stdout, delimiter=ARGS.delimiter)
 
 # Initialize correct loader for selected scanfile type
 loader_cls = FORMAT_LOADERS[ARGS.format]
 loader = loader_cls(ARGS.scan_dir)
-print(loader.scanfiles_by_date())
+
+# Filter
+not_before_dt = util.str2dt(ARGS.not_before) if ARGS.not_before is not None else None
+not_after_dt = util.str2dt(ARGS.not_after) if ARGS.not_after is not None else None
+loader.filter(not_before_dt, not_after_dt)
+
+# Downsample
+TIME_RE = re.compile('[0-9]{2}:[0-9]{2}:[0-9]{2}(,[0-9]{2}:[0-9]{2}:[0-9]{2})*')
+if TIME_RE.fullmatch(ARGS.downsample.strip()):
+    downsample_targets = ARGS.downsample.strip().split(',')
+    logging.info("Downsampling to times: %s", ", ".join(downsample_targets))
+    loader.downsample(targets=downsample_targets)
+
+# Sort by time
+loader.sort()
+
+# Produce output
+for iso_sf in loader.scanfiles_and_isotimes():
+    writer.writerow(iso_sf)
+
+logging.debug("===FINISH===")
---
+++ util.py
@@ -0,0 +1,26 @@
+import re
+from datetime import datetime
+
+LOG_FMT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
+
+def time2dt(timestr:str, daystr:str):
+    """timestr should be 24-hour time string in format HH:MM:SS
+    daystr should be in format YYYY-MM-DD."""
+    isoutc = "{}T{}Z".format(daystr, timestr)
+    return datetime.strptime(isoutc, "%Y-%m-%dT%H:%M:%SZ")
+
+def str2dt(dtstr:str):
+    just_date = re.compile("[0-9]{4}(-[0-9]{2}){2}")
+    date_and_time = re.compile("[0-9]{4}(-[0-9]{2}){2}T[0-9]{2}(-[0-9]{2}){2}Z?")
+
+    if just_date.fullmatch(dtstr):
+        fstr = "{}T{}Z".format(dtstr, "00:00:00")
+        return datetime.strptime(fstr, "%Y-%m-%dT%H:%M:%SZ")
+
+    elif date_and_time.fullmatch(dtstr):
+        fstr = dtstr
+        if not fstr.endswith("Z"):
+            fstr += "Z"
+        return datetime.strptime(fstr, "%Y-%m-%dT%H:%M:%SZ")
+
+    raise ValueError("Invalid date/time string: " + str(dtstr))
