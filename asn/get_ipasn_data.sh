064b28004ba113cb35f50bf303637a4efcc3d569
Lexi Brent <lexi.brent@sydney.edu.au>
2019-06-18 16:58:03 +1000

    compress

--- asn/get_ipasn_data.sh
+++ asn/get_ipasn_data.sh
@@ -5,4 +5,4 @@ NOT_AFTER="2019-06-17"
 
 python3 generate_pyasn_dates.py --not-before $NOT_BEFORE --not-after $NOT_AFTER > dates_to_download
 pyasn_util_download.py --dates-from-file dates_to_download
-pyasn_util_convert.py --bulk $NOT_BEFORE $NOT_AFTER
+pyasn_util_convert.py --compress --bulk $NOT_BEFORE $NOT_AFTER
