4f494dcb865bf408fd4a847cb080053402bde2d7
Lexi Brent <lexi.brent@sydney.edu.au>
2019-06-18 15:12:51 +1000

    stuff

--- scripts/make_cymru_lookup.py
+++ scripts/make_cymru_lookup.py
@@ -8,9 +8,9 @@ csv.field_size_limit(sys.maxsize)
 reader = csv.reader(sys.stdin, delimiter="\t")
 writer = csv.writer(sys.stdout, delimiter=" ")
 
-writer.writerow(("begin",))
-writer.writerow(("verbose",))
+# writer.writerow(("begin",))
+# writer.writerow(("verbose",))
 for (date, ips) in reader:
   for ip in ips.split(';'):
     writer.writerow((ip, date,))
-writer.writerow(("end",))
+# writer.writerow(("end",))
