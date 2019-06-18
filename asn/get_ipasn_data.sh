44c44082f9f13f3d6711c0bbe8d90d1f978331e2
Lexi Brent <lexi.brent@sydney.edu.au>
2019-06-18 16:11:56 +1000

    xattr

--- asn/get_ipasn_data.sh
+++ asn/get_ipasn_data.sh
@@ -3,6 +3,6 @@
 NOT_BEFORE="2019-06-15"
 NOT_AFTER="2019-06-17"
 
-./generate_pyasn_dates.py --not-before $NOT_BEFORE --not-after $NOT_AFTER > dates_to_download
+python3 generate_pyasn_dates.py --not-before $NOT_BEFORE --not-after $NOT_AFTER > dates_to_download
 pyasn_util_download.py --dates-from-file dates_to_download
 pyasn_util_convert.py --bulk $NOT_BEFORE $NOT_AFTER
