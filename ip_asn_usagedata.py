f3fed00be4f9d70dd8fb6827381192c9f99f47ab
Lexi Brent <lexi.brent@sydney.edu.au>
2019-11-26 11:01:46 +1100

    update ip_asn_usagedata

--- ip_asn_usagedata.py
+++ ip_asn_usagedata.py
@@ -6,7 +6,9 @@ import util
 
 from ip2location_db import lookup
 
-as_names = json.loads(open("asnames.dat").read())
+util.IPASN_DIR = "/net/gura/srv/hdd/pyasn/ipv4"
+
+as_names = json.loads(open("../notebooks/asnames.dat").read())
 
 def as_name(asnum):
   if asnum is None:
@@ -15,16 +17,16 @@ def as_name(asnum):
     return as_names[str(asnum)]
   return "Unknown"
 
-iploc = lookup.IP2Loc("./ip2location_db/ip2location.sqlite")
+ipl = lookup.IP2Loc("../ip2location.sqlite")
 
 ASNDATE = sys.argv[1]
 
-outwriter = csv.writer(sys.stdout, lineterminator="\n")
+outwriter = csv.writer(sys.stdout, lineterminator="\n", delimiter="\t")
 
 for ip in sys.stdin:
   ip = ip.strip()
   asn = util.ip2asn(ip, ASNDATE)
-  iploc = iploc.lookupv4(ip)
+  iploc = ipl.lookupv4(ip)
   outwriter.writerow((ip, asn, as_name(asn), 
     iploc["country_name"], iploc["isp"], iploc["domain"], iploc["usage_type"],
     ))
